80022fa49ac82a476920ba37f253cb493a18ac50
hatoba29 <hatoba29@naver.com>
2022-05-25 17:54:57 +0900

    feat: Add Review page
    - OXButtons.tsx: Export Wrapper
    - Result.tsx: Rename Btn to LinkBtn and export LinkBtn
    - Result.tsx: Import BtnWrapper from OXButtons

---
+++ src/components/Review/Card.tsx
@@ -0,0 +1,45 @@
+import styled from 'styled-components'
+import { useRecoilValue } from 'recoil'
+
+import { problemsState } from '@/store/problems'
+
+interface ICard {
+  i: number
+  hidden: boolean
+}
+
+const Card = (props: ICard): JSX.Element => {
+  const problems = useRecoilValue(problemsState)
+  const { i, hidden } = props
+  const answer = problems[i].answer === 1 ? 'O' : 'X'
+  const answerClass = hidden ? 'hidden' : ''
+
+  return (
+    <Wrapper>
+      <Problem>{problems[i].problem}</Problem>
+      <Answer className={answerClass}>정답: {answer}</Answer>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.article`
+  width: 100%;
+  margin: 12px 0;
+  border: 1px solid black;
+  padding: 8px;
+`
+
+const Problem = styled.p``
+
+const Answer = styled.p`
+  margin-right: 8px;
+
+  text-align: right;
+  font-weight: bold;
+
+  &.hidden {
+    visibility: hidden;
+  }
+`
+
+export default Card
